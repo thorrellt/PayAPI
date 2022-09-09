cceadf5f5989b38599a3751312caf2107ffd5384
Thorrell Turner <thorrellt@gmail.com>
2022-09-09 06:09:18 -0400

    add button styles

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ function App() {
       <h4>Hello, I am the H4</h4>
       <p>Hello, I am the P</p>
 
+      <button className='prim-btn'>Primary Button</button>
+      <button className='prim-btn' disabled>Disabled Primary Button</button>
+      <button className='sec-btn-dark'>Dark Button</button>
+      <button className='sec-btn-dark' disabled>Disabled Dark Button</button>
+      <button className='sec-btn-light'>Light Button</button>
+      <button className='sec-btn-light' disabled>Disabled Light Button</button>
+
     </div>
   )
 }
