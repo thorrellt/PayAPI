6bc7e54b61c50869f3efc1c4b657d2cc10314708
Thorrell Turner <thorrellt@gmail.com>
2022-09-10 10:04:38 -0400

    finish landing page mobile layout

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,20 +1,26 @@
 import { useState } from 'react'
 import '../../styles/home/Landing.css'
-import phoneMockup from '../../assets/home/desktop/illustration-phone-mockup.svg'
+import phoneMockup from '../../assets/home/desktop/illustration-phone-mockup2.png'
 
 
-export default function Landing(){
-    return(
+export default function Landing() {
+    return (
         <div className="Landing">
-            <img src={phoneMockup} 
-            alt="phone mockup"
-            className='phone-mockup' />
+            <div className="flex-container hero-img-wrapper">
+                <img src={phoneMockup}
+                    alt="phone mockup"
+                    className='phone-mockup' />
+            </div>
             <div className="cta">
-                <input 
-                type="email"
-                className='email'
-                placeholder='Enter email address' />
-                <button className='prim-btn'></button>
+                <h1 className=''>
+                    Start building with our APIs for absolutely free.
+                </h1>
+                <input
+                    type="email"
+                    className='email'
+                    placeholder='Enter email address' />
+                <button className='prim-btn'>Schedule a Demo</button>
+                <p>Have any questions? <a href="#">Contact Us</a></p>
             </div>
         </div>
     )
