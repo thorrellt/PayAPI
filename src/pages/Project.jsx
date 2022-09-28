e2955536ca410a6efb1044c6c845c4bd6d35572e
ThorrellT <thorrellt@gmail.com>
2022-09-28 07:11:09 -0400

    finish desktop about dev layout

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -8,45 +8,49 @@ export default function Project(props) {
     return (
         <main className='Project flex-container'>
             <h2>Hi, I'm Thorrell</h2>
-            <div className="img-container flex-container">
-                <img src={profilePic} alt="Thorrell's Picture" />
-                <Circle />
-            </div>
-            <p>
-                I'm a Washington Metropolitan based Software Developer transitioning into tech after 15+ years in public relations & customer service.
-            </p>
-            <div className="portfolio-wrapper flex-container">
-                <p>
-                    In order to learn more about me, view my work, or contact me, please visit my links below.
-                </p>
-                <a href="https://www.thorrellt.com/" target="_blank" rel="noopener noreferrer"
-                    title="www.thorrellt.com" >
-                    <button className="prim-btn">
-                        My Portfolio
-                    </button>
-                </a>
+            <section className='flex-container'>
+                <div className="img-container flex-container">
+                    <img src={profilePic} alt="Thorrell's Picture" />
+                    <Circle />
+                </div>
 
-            </div>
-            <div className="my-links flex-container">
-                <a href="https://github.com/thorrellt" target="_blank"
-                    rel="noopener noreferrer" title="github.com/thorrellt">
-                    <i className="bi bi-github" />
-                </a>
+                <div className="portfolio-wrapper flex-container">
+                    <p>
+                        I'm a Washington Metropolitan based Software Developer transitioning into tech after 15+ years in public relations & customer service.
+                    </p>
+                    <p>
+                        In order to learn more about me, view my work, or contact me, please visit my links below.
+                    </p>
+                    <a href="https://www.thorrellt.com/" target="_blank" rel="noopener noreferrer"
+                        title="www.thorrellt.com" >
+                        <button className="prim-btn">
+                            My Portfolio
+                        </button>
+                    </a>
 
 
-                <a href="https://www.linkedin.com/in/thorrellt/" target="_blank"
-                    rel="noopener noreferrer" title="linkedin.com/in/thorrellt">
-                    <i className="bi bi-linkedin" />
+                    <div className="my-links flex-container">
+                        <a href="https://github.com/thorrellt" target="_blank"
+                            rel="noopener noreferrer" title="github.com/thorrellt">
+                            <i className="bi bi-github" />
+                        </a>
 
-                </a>
 
-                <a href="mailto:thorrellt@gmail.com" target="_blank"
-                    rel="noopener noreferrer" title="thorrellt@gmail.com">
-                    <i className="bi bi-envelope" />
+                        <a href="https://www.linkedin.com/in/thorrellt/" target="_blank"
+                            rel="noopener noreferrer" title="linkedin.com/in/thorrellt">
+                            <i className="bi bi-linkedin" />
 
-                </a>
+                        </a>
 
-            </div>
+                        <a href="mailto:thorrellt@gmail.com" target="_blank"
+                            rel="noopener noreferrer" title="thorrellt@gmail.com">
+                            <i className="bi bi-envelope" />
+
+                        </a>
+                    </div>
+
+                </div>
+            </section>
         </main>
     )
 }
